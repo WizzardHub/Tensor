51449c2c2f9b9cf0f455b5bb7b51fa2cc5f5ed3f
Incognito <49486709+Incognitolol@users.noreply.github.com>
2025-03-26 15:03:39 -0400

    Chore: gitIgnore, and proper collections for large set of packets.

---
+++ src/main/java/dev/wizzardr/tensor/check/checks/AutoClickerB.java
@@ -0,0 +1,27 @@
+package dev.wizzardr.tensor.check.checks;
+
+import dev.wizzardr.tensor.check.factory.SwingCheck;
+import dev.wizzardr.tensor.check.factory.SwingCheckBuilder;
+import dev.wizzardr.tensor.data.PlayerData;
+
+import java.util.ArrayDeque;
+
+public class AutoClickerB extends SwingCheck {
+
+    public AutoClickerB(PlayerData playerData) {
+        super(playerData, SwingCheckBuilder.create()
+                .withName("Auto Clicker B")
+                .withSize(2)
+                .build());
+    }
+
+    @Override
+    protected void handle(ArrayDeque<Integer> samples) {
+        double cps = getCps();
+
+        if (cps > 19) {
+            debug(String.format("cps: %.2f", cps));
+        }
+    }
+}
+
