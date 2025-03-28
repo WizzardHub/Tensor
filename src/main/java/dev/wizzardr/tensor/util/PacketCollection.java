b0abe6f52a16abd1e4a17518c382550e922dd776
Incognito <49486709+Incognitolol@users.noreply.github.com>
2025-03-27 22:27:29 -0400

    chore: Added more packet collections to stop the use of big if statements

--- src/main/java/dev/wizzardr/tensor/util/PacketCollection.java
+++ src/main/java/dev/wizzardr/tensor/util/PacketCollection.java
@@ -16,4 +16,22 @@ public class PacketCollection {
             PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION,
             PacketType.Play.Client.PLAYER_ROTATION
     );
+
+    public final Set<PacketTypeCommon> LOOK = ImmutableSet.of(
+            PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION,
+            PacketType.Play.Client.PLAYER_ROTATION
+    );
+
+    public final Set<PacketTypeCommon> POSITION = ImmutableSet.of(
+            PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION,
+            PacketType.Play.Client.PLAYER_POSITION
+    );
+
+    public final Set<PacketTypeCommon> PLACE = ImmutableSet.of(
+            PacketType.Play.Client.PLAYER_BLOCK_PLACEMENT
+    );
+
+    public final Set<PacketTypeCommon> ATTACK = ImmutableSet.of(
+            PacketType.Play.Client.INTERACT_ENTITY
+    );
 }
