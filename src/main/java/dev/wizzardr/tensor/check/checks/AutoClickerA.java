05270243a76e7a3078079926a8526d319f267824
Incognito <49486709+Incognitolol@users.noreply.github.com>
2025-03-29 09:51:35 -0400

    chore: Added a preset / example of all of our statistics / builder

--- src/main/java/dev/wizzardr/tensor/check/checks/AutoClickerA.java
+++ src/main/java/dev/wizzardr/tensor/check/checks/AutoClickerA.java
@@ -6,22 +6,47 @@ import dev.wizzardr.tensor.data.PlayerData;
 import dev.wizzardr.tensor.math.Statistics;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.List;
 
 public class AutoClickerA extends SwingCheck {
 
     public AutoClickerA(PlayerData playerData) {
         super(playerData, SwingCheckBuilder.create()
-                .withName("Auto Clicker A")
-                .withSize(20)
-                .clearSamplesWhenFull()
+                .withName("Auto Clicker B") // Name your check
+                .withSize(20) // The sample size of the clicker
+                .clearSamplesWhenFull() // Clears the samples when the queue is full
+                .excludeDoubleClicks() // Voids double-clicks from samples
+                .markAsExperimental() // Mark this check as an experimental check ("This still needs tests")
                 .build());
     }
 
+
+    // Below we will display all the statistics that are inside our math class and have a debug pre-set for you.
     @Override
     protected void handle(ArrayDeque<Integer> samples) {
         double cps = getCps();
         double stDev = Statistics.getStDev(samples);
+        double modifCount = Statistics.getModifCount(samples);
+        double average = Statistics.getAverage(samples);
+        double entropy = Statistics.getEntropy(samples);
+        double gini = Statistics.getGini(samples);
+        double bds = Statistics.getBDS(samples);
+        double recurrenceRate = Statistics.getRecurrenceRate(samples);
+        double skewness = Statistics.getSkewness(samples);
+        double kurtosis = Statistics.getKurtosis(samples);
+        double variance = Statistics.getVariance(samples);
+        double variation = Statistics.getVariation(samples);
 
-        // debug(String.format("cps: %.2f stDev: %.2f", cps, stDev));
+        int[] distribution = Statistics.getDistribution(samples);
+
+        List<Integer> outlier = Statistics.getOutliers(samples);
+
+        debug(String.format(
+                "cps: %.2f, stDev: %.2f, modifCount: %.2f, average: %.2f, entropy: %.2f, gini: %.2f, " +
+                        "bds: %.2f, recurrenceRate: %.2f, skewness: %.2f, kurtosis: %.2f, variance: %.2f, " +
+                        "variation: %.2f, distribution: %s, outliers: %s",
+                cps, stDev, modifCount, average, entropy, gini, bds, recurrenceRate,
+                skewness, kurtosis, variance, variation, Arrays.toString(distribution), outlier.toString()));
     }
 }
