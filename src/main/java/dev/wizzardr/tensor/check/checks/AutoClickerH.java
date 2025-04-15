a1d713f5bfab4310e010e4c5adb47c4812049113
WizzardHub <contact@wizzardr.dev>
2025-04-15 15:09:25 +0200

    feat: added Auto Clicker H

---
+++ src/main/java/dev/wizzardr/tensor/check/checks/AutoClickerH.java
@@ -0,0 +1,43 @@
+package dev.wizzardr.tensor.check.checks;
+
+import dev.wizzardr.tensor.check.data.DebugContainer;
+import dev.wizzardr.tensor.check.factory.SwingCheck;
+import dev.wizzardr.tensor.check.factory.SwingCheckBuilder;
+import dev.wizzardr.tensor.data.PlayerData;
+import dev.wizzardr.tensor.math.Statistics;
+
+import java.util.ArrayDeque;
+
+/*
+ * Flags very low kurtosis patterns
+ */
+public class AutoClickerH extends SwingCheck {
+
+    public AutoClickerH(PlayerData playerData) {
+        super(playerData, SwingCheckBuilder.create()
+                .withName("Auto Clicker H")
+                .withSize(200)
+                .clearSamplesWhenFull()
+                .build());
+    }
+
+    @Override
+    protected void handle(ArrayDeque<Integer> sample) {
+
+        double cps = getCps();
+        double bds = Statistics.getBDS(sample);
+        double kurtosis = Statistics.getKurtosis(sample);
+
+        int oscillation = Statistics.getOscillation(sample);
+
+        DebugContainer data = DebugContainer.builder()
+                .formatString("cps: %.2f, kurtosis: %.2f, bds: %.2f, oscillation: %s")
+                .values(cps, kurtosis, bds, oscillation)
+                .build();
+
+        boolean bdsCompliant = Math.abs(100.0 - bds) > 0.5;
+        if (cps > 8 && kurtosis <= 0.25 && oscillation <= 1 && bdsCompliant) {
+            alert(data);
+        }
+    }
+}
